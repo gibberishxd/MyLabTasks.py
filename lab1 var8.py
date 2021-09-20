1e772dc29c6513df34f1c568a6cbf87c76274159
gibberishxd <gogiberidzetornike8@gmail.com>
2021-09-20 15:30:30 +0300

    added ifs.

--- lab1 var8.py
+++ lab1 var8.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 #Обчислити об'єми циліндра і конуса, що мають однакову висоту H і однаковий радіус основи R
 
@@ -6,6 +7,14 @@ print("Enter height ")
 h = input()
 print("Enter Radius ")
 r = input()
+
+if float(h) <= 0:
+    print("Height can't be zero or less")
+    sys.exit()
+if float(r) <= 0:
+    print("Radius can't be zero or less")
+    sys.exit()
+
 
 S = float(math.pi) * float(r) ** 2 * float(h)
 r = S / 3
